a215af02d462981e635d67d37bf7cc938e95b67b
arjun-1510 <Arjung1510@gmail.com>
2021-12-02 13:35:58 +0530

    node man

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Welcome to our home page')
-      } else if (req.url === '/about') {
-        res.end('Here is our short history')
-      } else {
-        res.end(`
-        <h1>Oops!</h1>
-        <p>We can't seem to find the page you are looking for</p>
-        <a href="/">back home</a>
-        `)
-      }
-    })
-    
-    server.listen(5000)
+
+ if (req.url ==='/') {
+   res.end('Home Page')
+ }
+  if (req.url === '/about') {
+   res.end('Our history')
+  }
+  
+})
+
+server.listen(5000, () => {
+  console.log('Server listening on port 5000......')
+})
 
 
