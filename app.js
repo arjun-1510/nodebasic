9fcc102d9ab7683b77654c019b58be69fde13204
arjun-1510 <Arjung1510@gmail.com>
2021-12-04 13:11:18 +0530

    crud

--- app.js
+++ app.js
@@ -1,64 +1,57 @@
-const express = require('express');
+
+const express = require('express')
 const app = express()
-const {products} = require('./data')
+let { people } = require('./data')
 
-app.get('/', (req,res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">Products</a>')
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
 })
 
-app.get('/api/products',(req,res) => {
-  const newProducts = products.map((product) => {
-    const {id , name, image } = product
-    return{id, name , image}
-  })
-  res.json(newProducts)
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
 })
-
-
-app.get('/api/products/:productID',(req,res) => {
-  const {productID} = req.params
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
   
- if (!singleProduct) {
-   return res.status(404).send("product Does Not Exit")
- }
-
- return res.json(singleProduct)
+app.post('/api/people', (req,res) => {
+  const {name} = req.body
+  if(!name) {
+    return res.status(400).json({success:false,msg:'please provide name value'})
+  }
+  res.status(200).send({success:true,person:name})
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
-  console.log(req.params);
-  res.send('hellow world')
-})
-
-
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
   }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
+  res.status(201).json({ success: true, data: [...people, name] })
 })
 
 
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
 
-app.listen(5000, () =>{
-  console.log('server is listen 5000 port ......')
+  res.status(401).send('Please Provide Credentials')
+})
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
